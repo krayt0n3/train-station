631a4b58ac725c0421fa5c36c524f67762065604
Krayt0n3 <hendersonkevin64@gmail.com>
2019-04-12 11:49:09 -0500

    need to delete js and start over

---
+++ assets/main.js
@@ -0,0 +1,12 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCmMU5GqooGJMGgAALH0uLKKo7k4gOFkLo",
+    authDomain: "train-scheduling-122ea.firebaseapp.com",
+    databaseURL: "https://train-scheduling-122ea.firebaseio.com",
+    projectId: "train-scheduling-122ea",
+    storageBucket: "train-scheduling-122ea.appspot.com",
+    messagingSenderId: "155924952153"
+  };
+  
+  firebase.initializeApp(config);
