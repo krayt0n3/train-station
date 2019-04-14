1bee68af49e5d62b758656529501195a50f33287
r8yt <hendersonkevin64@gmail.com>
2019-04-14 17:14:17 -0500

    functionality almost done

--- assets/main.js
+++ assets/main.js
@@ -10,3 +10,46 @@
   };
   
   firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+
+  $("#submit").click(function(event) {
+    event.preventDefault();
+    let train = $("#train-name").val();
+    let destination = $("#destination").val();
+    let time = $("#train-time").val();
+    let frequency = $("#frequency").val();
+  
+    database.ref().push({
+      train: train,
+      destination: destination,
+      frequency: frequency
+    });
+  });
+  
+  database.ref().on(
+    "child_added",
+    function(snapshot) {
+      console.log(snapshot.val());
+      var months = moment(snapshot.val().startDate, "MM/DD/YYYY");
+      var now = moment().format(date);
+      let today = moment();
+      let difference = today.diff(months, "months");
+      var totalBilled = difference * snapshot.val().monthlyRate;
+  
+      console.log(difference);
+  
+      $("tbody").append(`
+        <tr>
+            <td id="employee-name">${snapshot.val().train}</td>
+            <td id="place">${snapshot.val().destination}</td>
+            <td id="multiple">${snapshot.val().frequency}</td>
+            <hr>
+        </tr>
+      `);
+    },
+    function(errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    }
+  );
